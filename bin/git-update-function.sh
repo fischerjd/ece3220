fda735803c6b67c660c5248f5304efadfd4d481c
Jim Fischer <3539318+fischerjd@users.noreply.github.com>
2025-03-31 00:14:43 -0500

    feat(git-update): add cli flags -h and -r
    Option -h|--help
    Option -r|--restore-active-branch

--- bin/git-update-function.sh
+++ bin/git-update-function.sh
@@ -2,27 +2,41 @@
 # Copyright 2025 James D. Fischer
 # Source this file into the script that requires it.
 #
-# If you want this script to restore the original/starting branch after
-# it updates branches 'main' and 'develop', define an environment variable
-# named RESTORE_STARTING_BRANCH, e.g.,
-#
-#   $ RESTORE_STARTING_BRANCH=1 git update
-#
 
 ###########################################################################
-##  git_update()
+# git_update()
+#
+# @param[in] git_repo_directory
+# @param[in] restore_starting_directory (boolean, optional). Default value
+#       is 'false'.
+# 
+# DESCRIPTION
+#   For branches 'main' and 'develop' within the Git repository located
+#   in the specified git_repo_directory, git_update performes these tasks:
+#   - git switch <branch>
+#   - <stash any changed and/or untracked files>
+#   - git pull
+#   - <unstash any changed and/or untracked files>
+#
 ###########################################################################
 
 function git_update()
 {
     local git_repo_directory="${1%/}"
+    local restore_starting_branch="${2:-false}"
+    local branch_name
+    local git_repo_directory_realpath
+    local git_stash_starting_branch
+    local git_stash_current_branch
+    local starting_branch
     local -r GIT=/usr/bin/git
+    local -r REALPATH=/usr/bin/realpath
 
     # If the path string beings with a tilde character '~', replace the
     # tilde with the canonical path to the user's HOME directory.
     # See also:  https://stackoverflow.com/a/27485157/5051940
     git_repo_directory="${git_repo_directory/#\~/$HOME}"
-    git_repo_directory_realpath="$(/usr/bin/realpath "$git_repo_directory")"
+    git_repo_directory_realpath="$("${REALPATH}" "$git_repo_directory")"
 
     echo
     echo "==============================================================="
@@ -42,27 +56,53 @@ function git_update()
 
     pushd "$git_repo_directory" >/dev/null
 
-    if [ -n "${RESTORE_STARTING_BRANCH:+x}" ]; then
-        starting_branch="$("$GIT" branch --show-current)"
+    starting_branch=$("$GIT" branch --show-current)
+
+    # If the starting branch has changed or untracked files, stash them
+    # before continuing.
+    git_stash_starting_branch=false
+    if [[ $("$GIT" status --porcelain) ]]; then
+        "$GIT" stash --include-untracked
+        git_stash_starting_branch=true
     fi
 
     "$GIT" fetch --all
 
-    # Update branches 'main' and 'develop', in that order.
     for branch_name in main develop; do
-        if "$GIT" show-ref --quiet "refs/heads/${branch_name}"; then
+        if "$GIT" show-ref --quiet refs/heads/${branch_name}; then
             if "$GIT" switch "${branch_name}"; then
+
+                # If this branch has changed or untracked files, stash them.
+                git_stash_current_branch=false
+                if [[ $("$GIT" status --porcelain) ]]; then
+                    "$GIT" stash --include-untracked
+                    git_stash_current_branch=true
+                fi
+
                 "$GIT" pull
+
+                if $git_stash_current_branch; then
+                    "$GIT" stash pop
+                fi
             fi
         else
             >&2 echo ":: NOTICE :: Branch '${branch_name}' doesn't exist; skipping..."
         fi
     done
 
-    if [ -n "${RESTORE_STARTING_BRANCH:+x}" ]; then
-        "$GIT" switch "$starting_branch"
+    # Checkout the starting branch and pop any stashed files
+    "$GIT" checkout "$starting_branch"
+    if $git_stash_starting_branch; then
+        "$GIT" stash pop
     fi
-    
+
+    # If restore_starting_branch != true, switch to the 'develop' branch
+    # and exit. Otherwise, remain on this branch (the starting branch) and
+    # exit.
+    if [ "$restore_starting_branch" != "true" ]; then
+        "$GIT" switch develop
+    fi
+
     popd >/dev/null
 }
 
